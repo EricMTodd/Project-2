10a23872ab897db5b5a18b6bd20cc8e8dbe67ae8
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-07-27 10:10:21 -0600

    DEPLOY DAMMIT

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 
 // Create the db and connect
-let mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/gladiator";
+// let mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/gladiator";
 mongoose.connect(mongoUri);
 // const url = "mongodb://localhost:27017/gladiator";
 // mongoose.connect(url, { useNewUrlParser: true });
